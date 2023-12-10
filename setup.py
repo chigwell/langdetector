3003242533deea590c768d70863031bfc51bca77
E. Evstafiev <ee345@cam.ac.uk>
2023-12-10 13:55:57 +0000

    Add readme to setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,11 @@
 from setuptools import setup, find_packages
 
+with open('README.md', encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
     name='LngDetectoR',
-    version='0.1',
+    version='0.11',
     packages=find_packages(),
     install_requires=[
         'prettytable',
@@ -18,4 +21,6 @@ setup(
     description='A tool to detect programming languages in a directory.',
     keywords='language detection programming report',
     url='https://github.com/chigwell/langdetector',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
 )
