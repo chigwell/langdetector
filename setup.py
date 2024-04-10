eba34457ed31c1610b8692e6dc4f21fbfa3d6be6
E. Evstafiev <ee345@cam.ac.uk>
2024-04-10 11:57:16 +0100

    feat(lngdetector): add language detection functionality
    - Add `detect.py` for detecting programming languages in files and directories, supporting MIME types and file extensions.
    - Introduce `extension_lang.py` mapping file extensions to programming languages.
    - Implement `mime_lang.py` mapping MIME types to programming languages.
    - Add `report.py` for generating and printing reports on detected languages in directories.
    - Update `setup.py` entry point to `lngdetector = lngdetector.detect:main`, reflecting the new main module.
    - Increase package version to 0.16.
    
    fix: correct author name in `setup.py`
    
    - Correct the spelling of the author's name from 'Evgenii Evstafev' to 'Eugene Evstafev'.
    
    BREAKING CHANGE: remove `main.py` and `test.py`
    
    - Remove `main.py`, previously used as the entry point for language detection, replaced by `detect.py`.
    - Delete `test.py`, which contained tests dependent on the removed `main.py`. Users should update any scripts or automation tools that depend on these files.

--- setup.py
+++ setup.py
@@ -1,11 +1,13 @@
 from setuptools import setup, find_packages
 
+
 with open('README.md', encoding='utf-8') as f:
     long_description = f.read()
 
+
 setup(
     name='LngDetectoR',
-    version='0.15',
+    version='0.16',
     packages=find_packages(),
     install_requires=[
         'prettytable',
@@ -13,10 +15,10 @@ setup(
     ],
     entry_points={
         'console_scripts': [
-            'lngdetector = lngdetector.main:main',
+            'lngdetector = lngdetector.detect:main',
         ],
     },
-    author='Evgenii Evstafev',
+    author='Eugene Evstafev',
     author_email='chigwel@gmail.com',
     description='A tool to detect programming languages in a directory.',
     keywords='language detection programming report',
