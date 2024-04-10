de49ec688ca270b0e0ac8c19c22c9456b64068b2
E. Evstafiev <ee345@cam.ac.uk>
2024-04-10 14:05:13 +0100

    feat: update setup.py

--- setup.py
+++ setup.py
@@ -1,28 +1,38 @@
 from setuptools import setup, find_packages
 
 
-with open('README.md', encoding='utf-8') as f:
-    long_description = f.read()
-
-
 setup(
     name='LngDetectoR',
     version='0.16',
+    author='Eugene Evstafev',
+    author_email='chigwel@gmail.com',
+    description='A tool to detect programming languages in a directory.',
+    long_description=open('README.md', encoding='utf-8').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/chigwell/langdetector',
     packages=find_packages(),
     install_requires=[
         'prettytable',
         'python-magic',
     ],
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
     entry_points={
         'console_scripts': [
             'lngdetector = lngdetector.detect:main',
         ],
     },
-    author='Eugene Evstafev',
-    author_email='chigwel@gmail.com',
-    description='A tool to detect programming languages in a directory.',
-    keywords='language detection programming report',
-    url='https://github.com/chigwell/langdetector',
-    long_description=long_description,
-    long_description_content_type='text/markdown',
 )
