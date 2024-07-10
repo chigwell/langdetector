33b79dfd6eb763f274ea9c51918413450e69297f
E. Evstafiev <ee345@cam.ac.uk>
2024-07-10 16:58:50 +0100

    chore: black formatting

--- lngdetector/__init__.py
+++ lngdetector/__init__.py
@@ -1,7 +1,5 @@
-'''
+"""
 This is an initialization script for a Python package called 'LngDetectoR'.
 It sets up the necessary configurations and imports necessary modules.
 The package is used to detect programming languages in a directory using file mime types and extensions.
-'''
-
-
+"""
