5b37b88eac7fa28f5f250caefeeb259d4a0a0ea2
E. Evstafiev <ee345@cam.ac.uk>
2024-07-10 16:58:50 +0100

    feat(extension_lang): Add support for more file extensions
    The 'extension_to_language' dictionary in 'extension_lang.py' was updated to include additional file extensions and their corresponding programming languages. This enhancement provides improved language detection capabilities for a wider range of file types, supporting developers working on projects that utilize these additional languages.

---
+++ lngdetector/__init__.py
@@ -0,0 +1,7 @@
+'''
+This is an initialization script for a Python package called 'LngDetectoR'.
+It sets up the necessary configurations and imports necessary modules.
+The package is used to detect programming languages in a directory using file mime types and extensions.
+'''
+
+
