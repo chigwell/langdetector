d9d8763cc906f47da0dae6febda0684c8699e5a8
E. Evstafiev <ee345@cam.ac.uk>
2024-07-10 16:48:07 +0100

    Feat(lngdetector): Add language mappings for MIME types and extensions
    This commit adds new mappings to the 'mime_lang' and 'extension_lang' dictionaries in order to support identifying languages based on their MIME types and file extensions. The additions include Lisp, Scheme, Common Lisp, OCaml, F#, Coq, Isabelle, and Racket, improving the capabilities of the language detection module.

--- tests/test_detect.py
+++ tests/test_detect.py
@@ -1,3 +1,11 @@
+'''
+This file is a part of 'LngDetectoR', a tool to detect programming languages in a directory.
+It contains the implementation of the function 'detect_language' which determines
+the programming language of a given file based on its MIME type and extension. It uses Python's magic library for this.
+The 'mime_to_language' dictionary is used to map certain MIME types to their corresponding languages.
+'''
+   }
+
 import os
 from lngdetector.detect import detect_language, count_lines_of_code
 
