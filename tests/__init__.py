13ba9c40a36a30a50e6637b9afb17483f4fd47a7
E. Evstafiev <ee345@cam.ac.uk>
2024-07-10 16:50:48 +0100

    feat(lngdetector): Add mime type and extension language mappings
    Added mime type to language mappings in 'mime_lang.py' and extension to language mappings in 'extension_lang.py'. These changes provide more accurate language detection based on file types or MIME content types. Additionally, a comment was added to '__init__.py' for better documentation and understanding of the file.

---
+++ tests/__init__.py
@@ -0,0 +1,7 @@
+'''
+This file is a part of the LngDetectoR project and contains the function to detect the programming language of a file.
+It uses both MIME type and extension of the file to make an educated guess about its language.
+Additionally, it handles Git hook scripts and unknown extensions by returning 'Git Hook Script' or 'Unknown', respectively.
+'''
+
+
