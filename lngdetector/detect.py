9fe103a978701e4785ab1203d2e40565acb21e9d
E. Evstafiev <ee345@cam.ac.uk>
2024-07-10 17:00:38 +0100

    chore: black formatting

--- lngdetector/detect.py
+++ lngdetector/detect.py
@@ -1,9 +1,8 @@
-'''
+"""
 This Python script is a part of the 'LngDetectoR' tool and contains functions to detect the programming language of a file.
 It uses MIME types, extensions, and a dictionary mapping these elements to languages to make its predictions.
 The function 'detect_language(file_path)' identifies the language based on the file's MIME type and extension.
-'''
-
+"""
 
 import os
 import magic
@@ -17,7 +16,7 @@ from .extension_lang import extension_to_language
 def count_lines_of_code(file_path):
     """Count the number of lines in a file."""
     try:
-        with open(file_path, 'r', encoding='utf-8', errors='ignore') as f:
+        with open(file_path, "r", encoding="utf-8", errors="ignore") as f:
             return sum(1 for _ in f)
     except Exception:
         return None
@@ -29,16 +28,16 @@ def detect_language(file_path):
         mime_type = magic.from_file(file_path, mime=True)
         if mime_type in mime_to_language:
             return mime_to_language[mime_type]
-        if file_path.endswith('.sample') and '.git/hooks/' in file_path:
-            return 'Git Hook Script'
-        return extension_to_language.get(os.path.splitext(file_path)[1], 'Unknown')
+        if file_path.endswith(".sample") and ".git/hooks/" in file_path:
+            return "Git Hook Script"
+        return extension_to_language.get(os.path.splitext(file_path)[1], "Unknown")
     except Exception as e:
         return None
 
 
 def analyze_directory(directory):
     """Analyze all files in the given directory recursively and generate a report."""
-    languages = defaultdict(lambda: {'count': 0, 'lines': 0})
+    languages = defaultdict(lambda: {"count": 0, "lines": 0})
     total_files = 0
     total_lines = 0
 
@@ -49,8 +48,8 @@ def analyze_directory(directory):
             lines = count_lines_of_code(file_path)
             total_lines += lines or 0
             language = detect_language(file_path)
-            languages[language]['count'] += 1
-            languages[language]['lines'] += lines or 0
+            languages[language]["count"] += 1
+            languages[language]["lines"] += lines or 0
 
     return languages, total_files, total_lines
 
@@ -60,6 +59,7 @@ def generate_report(directory):
     languages, total_files, total_lines = analyze_directory(directory)
     return Report(languages, total_files, total_lines)
 
+
 def get_mime_types():
     """Return a list of all MIME types."""
     return list(mime_to_language.keys())
@@ -67,8 +67,10 @@ def get_mime_types():
 
 def main():
 
-    parser = argparse.ArgumentParser(description='Language Detection in a Directory.')
-    parser.add_argument('--directory', type=str, help='Directory to analyze', default=os.getcwd())
+    parser = argparse.ArgumentParser(description="Language Detection in a Directory.")
+    parser.add_argument(
+        "--directory", type=str, help="Directory to analyze", default=os.getcwd()
+    )
     args = parser.parse_args()
 
     report = generate_report(args.directory)
@@ -79,5 +81,5 @@ def main():
     # print(f"Python files: {python_data['count']}, lines of code: {python_data['lines']}")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
